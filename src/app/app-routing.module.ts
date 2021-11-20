238190071ed57b8ca273ec01507a1409190189a4
Akash <akashpandeyssm71@gmail.com>
2021-11-20 17:22:38 +0530

    Error component added for wildCard Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
 import { LoginComponent } from './login/login.component';
 import { StudentDetailsComponent } from './student-details/student-details.component';
 import { StudentComponent } from './student/student.component';
 
 const routes: Routes = [
   {path:"",redirectTo:"login",pathMatch:"full"},
+  {path:"login",component:LoginComponent},
   { path:"studentLink" , component:StudentComponent},
   {path:"studentDetailsLink",component:StudentDetailsComponent},
-  {path:"login",component:LoginComponent}
+  {path:"**",component:ErrorComponent}
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
 import { StudentDetailsComponent } from './student-details/student-details.component';
 import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentComponent,
     StudentDetailsComponent,
-    LoginComponent
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
