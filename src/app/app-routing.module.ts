be60adbe79af5ff833c937677d63b24f73a72766
Akash <akashpandeyssm71@gmail.com>
2021-11-20 17:13:37 +0530

    First Routing Tutorial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { StudentDetailsComponent } from './student-details/student-details.component';
+import { StudentComponent } from './student/student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"studentLink" , component:StudentComponent},
+  {path:"studentDetailsLink",component:StudentDetailsComponent},
+  {path:"login",component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
